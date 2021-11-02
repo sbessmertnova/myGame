dea5563730d148a5c25c4220bdf06fd6bc8a0957
sbessmertnova <minnij@yandex.ru>
2021-11-02 22:57:30 +0300

    added base

---
+++ WinFormsGame/WinFormsGame/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace WinFormsGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.kachinButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.klopButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.startImageButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // kachinButton
+            // 
+            this.kachinButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.kachinButton.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.kachinButton.Font = new System.Drawing.Font("Segoe Print", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.kachinButton.ForeColor = System.Drawing.Color.Red;
+            this.kachinButton.Location = new System.Drawing.Point(12, 20);
+            this.kachinButton.Name = "kachinButton";
+            this.kachinButton.Size = new System.Drawing.Size(500, 217);
+            this.kachinButton.TabIndex = 0;
+            this.kachinButton.Text = "Получить предсказание от Качина";
+            this.kachinButton.UseVisualStyleBackColor = false;
+            this.kachinButton.Click += new System.EventHandler(this.kachinButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(518, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(534, 566);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // klopButton
+            // 
+            this.klopButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.klopButton.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.klopButton.Font = new System.Drawing.Font("Segoe Print", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.klopButton.ForeColor = System.Drawing.Color.Red;
+            this.klopButton.Location = new System.Drawing.Point(518, 20);
+            this.klopButton.Name = "klopButton";
+            this.klopButton.Size = new System.Drawing.Size(522, 217);
+            this.klopButton.TabIndex = 2;
+            this.klopButton.Text = "Получить предсказание от Клопуса";
+            this.klopButton.UseVisualStyleBackColor = false;
+            this.klopButton.Click += new System.EventHandler(this.klopButton_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Controls.Add(this.kachinButton);
+            this.panel1.Controls.Add(this.klopButton);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 566);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1052, 249);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.OldLace;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.startImageButton);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(512, 566);
+            this.panel2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(506, 511);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Узнай, как пройдет \r\nтвой день";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.UseWaitCursor = true;
+            // 
+            // startImageButton
+            // 
+            this.startImageButton.Location = new System.Drawing.Point(344, 514);
+            this.startImageButton.Name = "startImageButton";
+            this.startImageButton.Size = new System.Drawing.Size(150, 46);
+            this.startImageButton.TabIndex = 0;
+            this.startImageButton.Text = "startImage";
+            this.startImageButton.UseVisualStyleBackColor = true;
+            this.startImageButton.Visible = false;
+            this.startImageButton.Click += new System.EventHandler(this.startImageButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1052, 815);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button kachinButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button klopButton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button startImageButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WinFormsGame/WinFormsGame/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void klopButton_Click(object sender, EventArgs e)
+        {
+            pictureBox1.ImageLocation = @"C:\Sveta\NIK\myGame\a.jpg";
+
+        }
+
+        private void kachinButton_Click(object sender, EventArgs e)
+        {
+            pictureBox1.ImageLocation = @"C:\Sveta\NIK\myGame\k.jpg";
+
+        }
+
+        private void startImageButton_Click(object sender, EventArgs e)
+        {
+            pictureBox1.ImageLocation = @"C:\Sveta\NIK\myGame\a.jpg";
+        }
+    }
+}
