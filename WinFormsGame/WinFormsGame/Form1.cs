50e7e20997b341dbb3011164279dc16cf6f12133
sbessmertnova <minnij@yandex.ru>
2021-11-05 13:57:24 +0300

    added logic

--- WinFormsGame/WinFormsGame/Form1.cs
+++ WinFormsGame/WinFormsGame/Form1.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Serialization;
 
 namespace WinFormsGame
 {
@@ -16,23 +18,30 @@ namespace WinFormsGame
         {
             InitializeComponent();
         }
-
-
+        
         private void klopButton_Click(object sender, EventArgs e)
         {
             pictureBox1.ImageLocation = @"C:\Sveta\NIK\myGame\a.jpg";
-
+            var prediction = LoadPredictionFromFile(AppDomain.CurrentDomain.BaseDirectory + "KlopPrediction.xml");
+            label1.Text = prediction.TextOfPrediction;
         }
 
         private void kachinButton_Click(object sender, EventArgs e)
         {
             pictureBox1.ImageLocation = @"C:\Sveta\NIK\myGame\k.jpg";
-
+            var prediction = LoadPredictionFromFile(AppDomain.CurrentDomain.BaseDirectory + "KachinPrediction.xml");
+            label1.Text = prediction.TextOfPrediction;
         }
 
-        private void startImageButton_Click(object sender, EventArgs e)
+        public static Prediction LoadPredictionFromFile (string pathToFile)
         {
-            pictureBox1.ImageLocation = @"C:\Sveta\NIK\myGame\a.jpg";
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Prediction>), new XmlRootAttribute("Predictions"));
+            FileStream fileStream = new FileStream(pathToFile, FileMode.Open, FileAccess.Read);
+            var predictions = (List<Prediction>)xmlSerializer.Deserialize(fileStream);
+            fileStream.Close();
+            var random = new Random();
+            var prediction = predictions[random.Next(10)];
+            return prediction;
         }
     }
 }
