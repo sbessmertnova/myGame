cb02052c0dab4d32a5e7b7a7aff355635b6e064c
sbessmertnova <minnij@yandex.ru>
2021-11-11 23:30:18 +0300

    added datetime logic

--- WinFormsGame/WinFormsGame/Form1.cs
+++ WinFormsGame/WinFormsGame/Form1.cs
@@ -21,16 +21,46 @@ namespace WinFormsGame
         
         private void klopButton_Click(object sender, EventArgs e)
         {
-            pictureBox1.ImageLocation = @"C:\Sveta\NIK\myGame\a.jpg";
-            var prediction = LoadPredictionFromFile(AppDomain.CurrentDomain.BaseDirectory + "KlopPrediction.xml");
-            label1.Text = prediction.TextOfPrediction;
+            if(CanGetPredition(AppDomain.CurrentDomain.BaseDirectory + "cdtfkl.xml"))
+            {
+                pictureBox1.ImageLocation = @"C:\Sveta\NIK\myGame\a.jpg";
+                var prediction = LoadPredictionFromFile(AppDomain.CurrentDomain.BaseDirectory + "KlopPrediction.xml");
+                label1.Text = prediction.TextOfPrediction;
+            }
+            else
+            {
+                MessageBox.Show("Только по одному предсказанию в день от каждого медиума. Забугривайся завтра!", "Важно!", MessageBoxButtons.OK, 
+                    MessageBoxIcon.Information, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+            }
+
+        }
+
+        bool CanGetPredition(string pathToFile)
+        {
+            var curentDateTime = DateTime.Now;
+
+            if (!File.Exists(pathToFile))
+            {
+                File.WriteAllText(pathToFile, curentDateTime.ToString());
+                return true;
+            }
+            var dateFromFile = DateTime.Parse(File.ReadAllText(pathToFile));
+            return curentDateTime.Date != dateFromFile.Date;
         }
 
         private void kachinButton_Click(object sender, EventArgs e)
         {
-            pictureBox1.ImageLocation = @"C:\Sveta\NIK\myGame\k.jpg";
-            var prediction = LoadPredictionFromFile(AppDomain.CurrentDomain.BaseDirectory + "KachinPrediction.xml");
-            label1.Text = prediction.TextOfPrediction;
+            if (CanGetPredition(AppDomain.CurrentDomain.BaseDirectory + "cdtfka.xml"))
+            {
+                pictureBox1.ImageLocation = @"C:\Sveta\NIK\myGame\k.jpg";
+                var prediction = LoadPredictionFromFile(AppDomain.CurrentDomain.BaseDirectory + "KachinPrediction.xml");
+                label1.Text = prediction.TextOfPrediction;
+            }
+            else
+            {
+                MessageBox.Show("Только по одному предсказанию в день от каждого медиума. Давай заавтра ещё поболтаем!", "Важно!", MessageBoxButtons.OK,
+                MessageBoxIcon.Information, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+            }
         }
 
         public static Prediction LoadPredictionFromFile (string pathToFile)
